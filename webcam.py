e06600d406d83aeb6ca53bc02c89fc033d7d81b6
Kennedy Mindeman <kennedymindeman@gmail.com>
2024-01-17 21:33:23 -0600

    Add edge detection function

--- webcam.py
+++ webcam.py
@@ -2,26 +2,46 @@
 import cv2
 import numpy
 
+def main():
+    # define a video capture object
+    vid = cv2.VideoCapture(1)
 
-# define a video capture object
-vid = cv2.VideoCapture(1)
+    while True:
 
-while True:
+        # Capture the video frame
+        # by frame
+        ret, frame = vid.read()
+        edge_image = edge_detection(frame)
 
-    # Capture the video frame 
-    # by frame 
-    ret, frame = vid.read()
+        # Display the resulting frame
+        cv2.imshow('frame', numpy.fliplr(edge_image))
 
-    # Display the resulting frame
-    cv2.imshow('frame', numpy.fliplr(frame))
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    # the 'q' button is set as the 
-    # quitting button you may use any 
-    # desired button of your choice 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
 
-# After the loop release the cap object 
-vid.release()
-# Destroy all the windows 
-cv2.destroyAllWindows()
+
+def edge_detection(image):
+    # Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Blur the image for better edge detection
+    # what does gaussian blur do?
+    # https://docs.opencv.org/3.1.0/d4/d13/tutorial_py_filtering.html
+    blur = cv2.GaussianBlur(gray, (1, 1), 0)
+
+    # Run the edge detector on grayscale
+    edges = cv2.Canny(blur, 50, 150, apertureSize=3)
+
+    return edges
+
+
+if __name__ == "__main__":
+    main()
